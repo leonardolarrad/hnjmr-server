6d3d52086aae56391a399a217e33f33074759014
Christian Jose <christianjsanchezr2025@gmail.com>
2022-08-31 14:35:07 -0400

    agregando interceptador  y middleware para las peticiones

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerAdapter } from './common/adapters/logger.adapter';
+import { LoggingInterceptor } from './common/interceptors/logger.interceptor';
+import { AllExceptionFilter } from './common/middleware/Excepcion.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,7 +13,9 @@ async function bootstrap() {
       whitelist: true,
       forbidNonWhitelisted: true,
     })
-  )
+  );
+  app.useGlobalFilters(new AllExceptionFilter(new LoggerAdapter()));
+  app.useGlobalInterceptors(new LoggingInterceptor(new LoggerAdapter()));
   await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
