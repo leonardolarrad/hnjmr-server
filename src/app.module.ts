363bc089f7f59b6ea82a7f907db14360c7dd8395
Christian Jose <christianjsanchezr2025@gmail.com>
2022-09-13 23:20:09 -0400

    hotfix

--- src/app.module.ts
+++ src/app.module.ts
@@ -33,7 +33,7 @@ import { UserModule } from './user/user.module';
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
       autoLoadEntities: true,
-      synchronize: process.env.STAGE === 'dev',
+      synchronize: true,
     }),
     MedicalSupplyModule,
     LotModule,
